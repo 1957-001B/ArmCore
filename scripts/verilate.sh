0614a2c03cd3ef9ae4c469a4d241bfe0b9e95f43
Soyuz <michael1957001b@gmail.com>
2025-10-22 13:17:42 +0100

    Pc and halt logic

--- scripts/verilate.sh
+++ scripts/verilate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Users/camus/build/verilator/bin/verilator_bin --cc --exe --build --trace -I./src testbenches/cpu_tb.v src/cpu.v --top-module cpu_tb -o sim_cpu #--timing
+verilator --cc --Wall ./testbenches/sim_main.cpp --exe --build --trace -I./src testbenches/cpu_tb.v src/cpu.v --top-module cpu_tb -o sim_cpu --timing
--- src/cpu.v
+++ src/cpu.v
@@ -3,17 +3,16 @@
 `include "params.vh"
 
 module cpu (
-    input clk,
-    input reset,
+    input wire clk,
+    input wire reset,
+    input wire test_halt,
     output reg [63:0] pc,
-    output wire halted
+    output reg halted
   );
 
 // Will be set by the bootloader
 localparam INITPC =  64'h0;
 // localparam INITSP = 1000;
-
-assign halted = 0;
 
 // Instruction Memory
 wire [31:0] instruction;
@@ -42,98 +41,79 @@ wire [63:0] Write_mem_d;
 // Register File 
 wire [4:0] Read_register_1;
 wire [4:0] Read_register_2;
+
 assign Read_register_1 = instruction[9:5];
 assign Read_register_2 = Reg2Loc ? instruction[20:16] : instruction[4:0];
+
 wire [4:0] Write_register;
+
 assign Write_register = instruction[4:0];
+
 wire [63:0] Write_d;
 wire [63:0] alu_result;
+
 assign Write_d = MemToReg ? alu_result : Read_mem_d;
+
 wire [63:0] Read_data_1;
 wire [63:0] Read_data_2;
 
-//Alu
+// APSR
+wire N, Z, C, V;
+// Alu
 wire alu_zero;
 wire mod_flags; //not sure if I will input flag branches
-wire N, Z, C, V;
 wire [63:0] alu_mux;
 assign alu_mux = ALUSrc ? Read_data_2 : padded_imm;
 
 // Alu Control
 wire [3:0] alu_op;
 
-// Output the current instruction at the PC.
-imem instruction_memory(
-  .pc(pc),
-  .instruction(instruction),
-  .reset(reset)
+// Module Instantiation
+control u_control (
+    .instruction     (instruction),
+    // Control flags
+    .Reg2Loc         (Reg2Loc),
+    .UncondBranch    (UncondBranch),
+    .FlagBranch      (FlagBranch),
+    .ZeroBranch      (ZeroBranch),
+    .MemRead         (MemRead),
+    .MemToReg        (MemToReg),
+    .MemWrite        (MemWrite),
+    .FlagWrite       (FlagWrite),
+    .ALUSrc          (ALUSrc),
+    .ALUOp           (ALUOp),
+    .RegWrite        (RegWrite),
+    .UseSP           (UseSP)
 );
 
-dmem data_memory(
-  .clk(clk),
-  .address(alu_result),
-  .MemWrite(MemWrite),
-  .MemRead(MemRead),
-  .Read_d(Read_mem_d),
-  .Write_d(Write_mem_d)
+apsr u_apsr (
+    .result       (result),
+    .flagWrite    (flagWrite),
+    .N            (N),
+    .Z            (Z),
+    .C            (C),
+    .V            (V)
 );
 
-control control_unit(
-  .instruction(instruction),
-  .Reg2Loc(Reg2Loc),
-  .UncondBranch(UncondBranch),
-  .FlagBranch(FlagBranch),
-  .ZeroBranch(ZeroBranch),
-  .MemRead(MemRead),
-  .MemToReg(MemToReg),
-  .MemWrite(MemWrite),
-  .FlagWrite(FlagWrite),
-  .ALUSrc(ALUSrc),
-  .ALUOp(ALUOp),
-  .RegWrite(RegWrite),
-  .UseSP(UseSP)
+alu u_alu (
+    .zero      (zero),
+    .A         (A),
+    .B         (B),
+    .alu_op    (alu_op),
+    .result    (result)
 );
 
-reg_file registers( 
-  .clk(clk),
-  .reset(reset),
-  .RegWrite(RegWrite),
-  .Read_register_1(Read_register_1),
-  .Read_register_2(Read_register_2),
-  .Write_register(Write_register),
-  .Write_d(Write_d),
-  .Read_data_1(Read_data_1),
-  .Read_data_2(Read_data_2),
-  .UseSP(UseSP)
+alu_control u_alu_control (
+    .instruction    (instruction), // Instruction [31:22]
+    .ALUOp          (ALUOp), //from control unit
+    .alu_op         (alu_op) //to alu
 );
 
-pad pad(
-  .instruction(instruction),
-  .padded_inst(padded_imm)
+pad u_pad (
+    .instruction    (instruction),
+    .padded_inst    (padded_inst)
 );
 
-alu_control aluctrl(
-  .instruction(instruction),
-  .ALUOp(ALUOp),
-  .alu_op(alu_op)
-);
-
-alu alu(
-  .zero(alu_zero),
-  .A(Read_data_1),
-  .B(alu_mux),
-  .alu_op(alu_op),
-  .result(alu_result)
-);
-
-apsr apsr(
-    .result(alu_result),
-    .flagWrite(FlagWrite),
-    .N(N),
-    .Z(Z),
-    .C(C),
-    .V(V)
-);
 
 //  PC logic
 wire [63:0] pc_norm, pc_jump, pc_mux;
@@ -144,12 +124,24 @@ assign pc_jump = pc + (padded_imm << 2);
 assign pc_select = UncondBranch | FlagBranch | (ZeroBranch & alu_zero);
 assign pc_mux = pc_select ? pc_jump : pc_norm;
 
+// PC and halt logic
 always @(posedge clk or posedge reset) begin
     if (reset) begin
+      // Reset signal driven by tb 
       pc <= INITPC;
-      end else if (!halted) begin  
-        pc <= pc_mux;
-      end
-    end 
+      halted <= 1b'0;
+    end else if (halted) begin  
+      pc <= pc;
+    end else if (test_halt) begin
+    // Halt when test_halted is high
+      pc <= pc;
+    end else if (req_halt) begin 
+      halted <= 1b'1;
+      pc <= pc;
+    end else begin
+      // Normal Operation
+      pc <= pc_mux;
+    end
+  end
 
 endmodule
