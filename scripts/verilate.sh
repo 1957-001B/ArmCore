0a239f73c9344de96cf2db6de76fe2c213b59b1f
1957-001B <michael1957001b@gmail.com>
2025-11-06 13:22:28 +0000

    ./scripts/verilate now has VIZ=1 option to open GTKwave and properly generates traces

--- scripts/verilate.sh
+++ scripts/verilate.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-verilator --cc --Wall ./testbenches/sim_main.cpp --exe --build --trace -I./src testbenches/cpu_tb.v src/cpu.v --top-module cpu_tb -o sim_cpu --timing
+set -euo pipefail  
+
+verilator --cc --Wall ./testbenches/sim_main.cpp \
+--exe  \
+--build \
+--trace \
+--trace-vcd \
+-I./src testbenches/cpu_tb.v src/cpu.v \
+--top-module cpu_tb \
+-o sim_cpu \
+--timing \
+&& ./obj_dir/sim_cpu +trace
+
+if [[ "${VIZ}" = "1" ]]; then
+  gtkwave waveform.vcd
+fi
+
